534fe97a302ce91b179c918aa9cd73d28aec9652
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2023-09-24 18:30:07 +0530

    Create conversion folder

---
+++ 03_ConversionOPeration.js
@@ -0,0 +1,33 @@
+let score= "33abc"
+console.log(typeof score);
+let valueInNumber = Number(score)  // convert string to number 
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let score1= "33"
+console.log(typeof score1);
+let valueInNumber1 = Number(score1)  // convert string to number 
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+
+/* conversion
+"33"= 33
+"33abc"=NaN
+true= 1 ,false=0
+*/
+
+let isLoggedIn= 1
+ let booleanIsLooggedIn= Boolean(isLoggedIn)
+ console.log(booleanIsLooggedIn);
+
+ /*
+ 1 = true , 0= false
+ ""= false
+ "hitesh"= true
+ */
+
+ let someNumber = 33
+ let stringNumber= String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
