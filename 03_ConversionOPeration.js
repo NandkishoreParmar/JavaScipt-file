74ff111622459bb2642f9e524b849b99f4541899
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2023-09-24 20:23:11 +0530

    add some more conversion

--- 03_ConversionOPeration.js
+++ 03_ConversionOPeration.js
@@ -1,3 +1,5 @@
+//************Conversion**********
+// Ecma 262 // https://tc39.es/ecma262/#sec-abstract-operations
 let score= "33abc"
 console.log(typeof score);
 let valueInNumber = Number(score)  // convert string to number 
@@ -31,3 +33,37 @@ let isLoggedIn= 1
  let stringNumber= String(someNumber)
  console.log(stringNumber)
  console.log(typeof stringNumber)
+
+ //****************Operations******************
+
+
+ let value = 3
+ let NegValue = -value
+ console.log(NegValue);
+
+ //console.log(2+2)
+ //console.log(2-2)
+ //console.log(2*2)
+ //console.log(2**3);
+ //console.log(2/3);
+ //console.log(2%3);
+
+ let str1= "hello"
+ let str2= " nandkishore "
+ let str3= str1 + str2
+ console.log(str3);
+
+ console.log("1" + 2); // 12
+ console.log(1 + "2"); // 12
+ console.log("1" + 2 + 2);//122
+ console.log(1 + 2 + "2");//32
+
+ console.log((3+4) * 5 % 3);
+ console.log(+true );//1
+  
+ let gameCounter = 100
+ ++gameCounter
+ console.log(gameCounter);
+ 
+ //link to study
+ //https://tc39.es/ecma262/#sec-abstract-operations
