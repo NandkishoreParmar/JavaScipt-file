99324dec73d7d5e83cf05326850c7946ec2b858a
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2023-09-24 09:01:56 +0530

    "create new folder"

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountId  = 14453
+let accountEmail = "nandkishoreparmar97@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur  "
+
+console.log(accountId);
+accountEmail="naandkisgoe@gmail.com"
+accountPassword="87654"
+accountCity= "bhopal"
+let accountState
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState ])
---
+++ 02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict "; // treat all js code as newer version
+
+//alert (3 + 3) //we are using nodejs, not browser
+
+console.log(3
+    
+    //code read karne me sahii hona chahiye  ase likhna sahi hota hy // console.log(3+3)
+
+    + 3)// code ase nhi likhna hy 
+
+    console.log("Nandkishore ");
+
+
+
+    //Ecma standards  (padne ke liye)
+    //mdn  (padne ke liye )
+    
+    
+    let name = "Nandkishore " // string datatype ho gya 
+    let age = 18 // integer datatype ho gya 
+    let isLoggedIn= true // boolean ho gya
+
+    /* Datatypes 
+    number = 2 to power 53
+    bigint = value number datatype se bhi badi ho 
+    string = ""
+    boolean =true/false 
+    null = standalone value // value zero ho 
+    undefined = variable declare kiya pr value assign nhi kari hy 
+    symbol = unique hona 
+    */
+
+    console.log(typeof age) // typeoff kisi bhi variable ka datatype check karne ke liye use karte
+    console.log(typeof null)// output object 
+    console.log(typeof undefined)// output undefined
+    
